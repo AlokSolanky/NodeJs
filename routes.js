b1aa8ba58cba3e3109c5778d8480dfc24ab830e8
AlokSolanky <61519147+AlokSolanky@users.noreply.github.com>
2023-08-03 03:09:06 +0530

    Create routes.js
    created seperate file for routes handling

---
+++ routes.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+function handleRoute(req,res)
+{
+    if (req.url === "/") {
+      let d = fs.readFileSync("message.txt", "utf-8");
+      res.write(`<body>`);
+      res.write(`<p>${d}</p>`);
+      res.write(
+        `<form method="POST" action="/message"><input type="text" name="message" required><button type="submit">Send</button></form>`
+      );
+      res.write(`</body>`);
+    } else if (req.url === "/message" && req.method === "POST") {
+      const body = [];
+      req.on("data", (chunk) => {
+        body.push(chunk);
+      });
+      req.on("end", () => {
+        const parsed = Buffer.concat(body).toString().split("=")[1];
+        fs.writeFileSync("message.txt", parsed);
+      });
+
+      res.statusCode = 302;
+      res.setHeader("Location", "/");
+    }
+    res.end();
+}
+
+module.exports = handleRoute;
