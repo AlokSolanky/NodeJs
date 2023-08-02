8dabf5354631f027de6cb56bf11b5958ef6e1fdf
AlokSolanky <61519147+AlokSolanky@users.noreply.github.com>
2023-08-03 01:28:29 +0530

    Update app.js
    3 routes managed (/,/about,/node)

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const http = require('http');
 http.createServer((req,res)=>
 {
-    res.write(`<h1> Welcome to my node js server</h1>`);
+    if(req.url === "/")
+    {
+        res.write(`<h1>Welcome Home</h1>`);
+    }
+    else if (req.url === "/about") {
+      res.write(`<h1> welcome to about page</h1>`);
+    }
+    else if (req.url === "/node") {
+      res.write(`<h1> welcome to my node js project</h1>`);
+    }
     res.end();
 }).listen(4000);
