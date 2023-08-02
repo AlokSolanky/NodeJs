f29b5237b12d12824391434a5dc7463f66b10ab7
AlokSolanky <61519147+AlokSolanky@users.noreply.github.com>
2023-08-03 02:20:48 +0530

    Update app.js
    now messages appears on home page when send

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
 const http = require('http');
+const fs = require('fs');
 http.createServer((req,res)=>
 {
     if(req.url === "/")
-    {
-        res.write(`<h1>Welcome Home</h1>`);
+    {   let d = fs.readFileSync("message.txt","utf-8");
+        res.write(`<body>`);
+        res.write(`<p>${d}</p>`);
+        res.write(`<form method="POST" action="/message"><input type="text" name="message" required><button type="submit">Send</button></form>`);
+        res.write(`</body>`);
     }
-    else if (req.url === "/about") {
-      res.write(`<h1> welcome to about page</h1>`);
-    }
-    else if (req.url === "/node") {
-      res.write(`<h1> welcome to my node js project</h1>`);
+    else if (req.url === "/message" && req.method === "POST") {
+      const body = [];
+      req.on('data',(chunk)=>
+      {
+        body.push(chunk);
+      });
+      req.on('end',()=>
+      {
+        const parsed = Buffer.concat(body).toString().split("=")[1];
+        fs.writeFileSync("message.txt", parsed);
+      })
+      
+      res.statusCode = 302;
+    //   res.write("Submitted");
+      res.setHeader('Location','/');
+      res.end();
     }
     res.end();
 }).listen(4000);
